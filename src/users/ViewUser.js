f1fda93c7b1fe2abb0b754d9184ee2980a0662c9
Santkumar Tigga <tiggasantkumar@gmail.com>
2025-05-14 13:42:48 +0530

    Initial commit for CRUDoperation Spring Boot project

---
+++ src/users/ViewUser.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, Link } from "react-router-dom";
+
+export default function ViewUser() {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+  // const { name, username, email } = user;
+  const { id } = useParams();
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    setUser(result.data);
+  };
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+            <h2 className="text-center m-4">User Details </h2>
+            <div className="card">
+              <div className="card-header">
+                Details of User ID:{user.id}
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    <b> Name:</b>
+                    {user.name}
+                  </li>
+                  <li className="list-group-item">
+                    <b> UserName:</b>
+                    {user.username}
+                  </li>
+
+                  <li className="list-group-item">
+                    <b> Email:</b>
+                    {user.email}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <Link className="btn btn-primary my-2" to={"/"}>
+              Back to Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">User Details </h2>
+          <div className="card">
+            <div className="card-header">
+              Details of User ID:{user.id}
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <b> Name:</b>
+                  {user.name}
+                </li>
+
+                <li className="list-group-item">
+                  <b> UserName:</b>
+                  {user.username}
+                </li>
+                <li className="list-group-item">
+                  <b> Email:</b>
+                  {user.email}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Link className="btn btn-primary my-2" to={"/"}>
+            Back to Home
+          </Link>
+        </div>
+      </div> */
+}
